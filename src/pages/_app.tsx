f9acaacf85fc0580ec60649934cdd412caef96ba
Trickey <vngthnh999@gmail.com>
2023-05-06 16:10:42 +0700

    Establish font body

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 // Create a client
 const queryClient = new QueryClient();
@@ -9,7 +15,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     // Provide the client to your App
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <div className={`${manrope.className}`}>
+        <Component {...pageProps} />
+      </div>
     </QueryClientProvider>
   );
 }
